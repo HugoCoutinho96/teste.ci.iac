84226c2d40a904bdf1d0ee7590cf91ef8e774319
Hugo Coutinho <hugocoutinho96@outlook.com>
2025-04-08 19:23:18 -0300

    novo: configure backend

--- main.tf
+++ main.tf
@@ -5,8 +5,36 @@ terraform {
       version = "5.94.1"
     }
   }
+
+  backend "s3" {
+    bucket = "projeto-iac-state"
+    key    = "state/terraform.tfstate"
+    region = "eu-central-1"
+  }
+  
 }
 
 provider "aws" {
     region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "projeto-iac-state"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Iac = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "projeto-iac-state"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
